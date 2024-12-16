7ff90cf8b7839c5135957cb4193aa6213563d415
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2024-12-15 21:53:49 -0800

    Update db.js

--- Models/db.js
+++ Models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongo_url = process.env.MONGO_URL;
+const mongo_url = 'mongodb+srv://arbab02:arbab02@cluster0.m3fklkq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 mongoose.connect(mongo_url)
     .then(() => {
